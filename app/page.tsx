99788dc6a9404e520b96c12610dbd1daf0410e57
MTahaSh <tahashayk2k@gmail.com>
2023-06-01 21:48:32 +0500

    changes in export

--- app/page.tsx
+++ app/page.tsx
@@ -5,36 +5,9 @@ import { client } from '@/sanity/lib/client'
 import Image from 'next/image'
 import { Image as IImage } from 'sanity'
 
-export const getData = async()=>
-{ 
-  const res = await client.fetch(`*[_type == 'products'] {
-    _id,
-    title,
-    description,
-    image,
-      category -> {
-        name
-      }
-  }`);
-  return res;
-
-}
-
-interface Iproduct {
-  _id: string
-  title: string,
-  description: string,
-  image: IImage,
-  price:number
-  category: {
-    name: string
-  }
-} 
 
 export default async function Home() {
 
-  const data: Iproduct[] = await getData();
-  console.log(data);
   
   return (
     <>
