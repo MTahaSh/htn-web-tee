f774f6ea63dd8d39bbeef314dc16a347920badeb
MTahaSh <tahashayk2k@gmail.com>
2023-06-01 18:45:47 +0500

    fixing key issue

--- app/Products/page.tsx
+++ app/Products/page.tsx
@@ -53,7 +53,7 @@ export default async function Products() {
 
 
       {data.map((item)=>(
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
+        <div className="lg:w-1/3 sm:w-1/2 p-4" key={item._id}>
         <div className="flex relative">
           <Image alt="gallery" className="absolute inset-0 w-full max-h-[300px] h-full object-contain object-center" src={urlForImage(item.image).url()} width={400} height={400}></Image>
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
--- app/Xbox/page.tsx
+++ app/Xbox/page.tsx
@@ -52,7 +52,7 @@ export default async function Xbox() {
       {data.map((item)=>(
 
         item.category.name === "Xbox"? 
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
+        <div className="lg:w-1/3 sm:w-1/2 p-4" key={item._id}>
         <div className="flex relative">
           <Image alt="gallery" className="absolute inset-0 w-full h-full max-h-[200px] object-contain object-center" src={urlForImage(item.image).url()} width={400} height={400}></Image>
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
