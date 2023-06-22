39b81fd7f085d19c863fe0df194a050068020a53
MTahaSh <tahashayk2k@gmail.com>
2023-06-22 19:46:40 +0500

    ..

--- app/Products/[...slug]/page.tsx
+++ app/Products/[...slug]/page.tsx
@@ -53,7 +53,7 @@ console.log(result);
   return (
     <div>
       {result.map((item)=>(
-        <section className="text-gray-600 body-font w-full overflow-hidden">
+        <section key={item._id} className="text-gray-600 body-font w-full overflow-hidden">
         <div className="container px-5 py-24 mx-auto flex">
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
             <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
