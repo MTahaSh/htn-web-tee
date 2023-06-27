1ceac65101a7beaba364125c9f44b7a4b5da738f
MTahaSh <tahashayk2k@gmail.com>
2023-06-27 20:19:19 +0500

    Update route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -185,13 +185,13 @@
 // // };
 
 
-import { getAuth } from "@clerk/nextjs/server";
-import type { NextApiRequest, NextApiResponse } from 'next'
+// import { getAuth } from "@clerk/nextjs/server";
+// import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // const { userId } = getAuth(req);
-  // console.log(userId);
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   // const { userId } = getAuth(req);
+//   // console.log(userId);
   
-    // Load any data your application needs for the API route
-  return res.status(200).json({name: "harry"})
-};
+//     // Load any data your application needs for the API route
+//   return res.status(200).json({name: "harry"})
+// };
