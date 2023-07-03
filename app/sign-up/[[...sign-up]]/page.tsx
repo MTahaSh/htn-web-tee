7217a83157afa4eb3c7462048386e88d6977ded1
MTahaSh <tahashayk2k@gmail.com>
2023-07-03 19:55:41 +0500

    some changes with sign up

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-  <div className="flex fixed bg-white justify-center items-center w-full">
+  <div className="flex fixed bg-white justify-center items-center w-full z-10">
   <SignUp />
   </div>
   )
